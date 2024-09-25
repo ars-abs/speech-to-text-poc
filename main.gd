6ccd435529ad9fd768617a88ec2dddc044f423df
ars-abs <114380406+ars-abs@users.noreply.github.com>
2024-09-25 18:59:45 +0530

    Todo completed: Asked permission on listen

--- main.gd
+++ main.gd
@@ -3,8 +3,6 @@ extends Node
 var stt
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# #NOTE: Ask for permission _on_listen_pressed only if permission is not in granted permissions.
-	OS.request_permission("RECORD_AUDIO")
 	if Engine.has_singleton("SpeechToText"):
 		stt = Engine.get_singleton("SpeechToText")
 		stt.setLanguage("en")
@@ -19,6 +17,7 @@ func onError(code):
 	$Control/TextEdit2.text = "error "+ code
 
 func _on_listen_pressed():
+	OS.request_permission("RECORD_AUDIO")
 	stt.listen()
 
 func _on_stop_pressed():
